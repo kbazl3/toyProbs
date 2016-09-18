6a1203799845aa254b58185e398b9d5a99e9c7e5
Kyle Barlow <kbarlow@kbarlow-l2.corp.instructure.com>
2016-09-17 22:16:16 -0600

    Finders Keepers toy prob

--- toyProbs.js
+++ toyProbs.js
@@ -29,3 +29,14 @@ function reverseString(str) {
 	str = str.split("");
   return str.reverse().join("");
 }
+
+
+//Create a function that looks through an array (first argument) and returns the first element in the array that passes a truth test (second argument).
+
+function findElement(arr, func) {
+  for (var i = 0; i < arr.length; i++) {
+      if (func(arr[i]) === true) {
+          return arr[i];
+      }
+  }
+}
